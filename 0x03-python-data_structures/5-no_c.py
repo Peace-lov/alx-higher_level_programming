a5a05b874bbe59f178bcd7a3837191d58ff94a47
Peace-lov <chidieberepeace42@gmail.com>
2023-11-06 16:19:45 -0800

    Python

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    char_to_move = ['c', 'C']
+    char_to_move = ""
     for a in my_string:
-        if a == char_to_move:
-            continue
-        else:
-            return my_string
+        if a != 'c' and a != 'C':
+            char_to_move += a
+        return char_to_move
