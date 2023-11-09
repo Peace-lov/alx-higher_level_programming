2c77e81ecf09c86c4948b9a95aa03a569ca4185c
Peace-lov <chidieberepeace42@gmail.com>
2023-11-08 18:39:01 -0800

    pthon2

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,23 +3,22 @@ if __name__ == "__main__":
     from sys import argv
     from calculator_1 import add, sub, mul, div
     arg_c = len(argv)
-    if atg_c != 4:
-        print('Usage: {} <a> <operator> <b>'.format
-                (argv[0]))
+    if arg_c != 4:
+        print('Usage: {} <a> <operator> <b>'.format(argv[0]))
         exit(1)
     ops = {
-            '+' : add,
-            '-' : sub,
-            '*' : mul,
-            '/' : div
-        }
+        '+': add,
+        '-': sub,
+        '*': mul,
+        '/': div
+    }
     if argv[2] in ops:
-        numb1 = int(argv[1])
-        numb2 = int(argv[3])
+        nums1 = int(argv[1])
+        nums2 = int(argv[3])
         op = ops[argv[2]]
-        res = op(numb1, numb2)
-        print('{:d} {:s} {:d} = {:d}'.format(numb1,
-            argv[2], numb2, res))
+        res = op(nums1, nums2)
+        print('{:d} {:s} {:d} = {:d}'.format(nums1,
+            argv[2], nums2, res))
     else:
         print('Unknown operator. Available operators: +, -,
         *, and /')
