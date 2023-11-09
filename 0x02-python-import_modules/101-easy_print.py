7814123af5ed8c53962cefa36c2d55f8ca07c9b8
Peace-lov <chidieberepeace42@gmail.com>
2023-11-08 18:23:18 -0800

    python2

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,11 +1,2 @@
 #!/usr/bin/python3
-from magic_calculation_102 import add, sub
-
-def magic_calculation(a, b):
-    if a < b:
-        cal = add(a, b)
-        for a in range(4, 6):
-            cal = add(cal, a)
-        return (cal)
-    else:
-        return (sub(a, b))
+import python_for_me
