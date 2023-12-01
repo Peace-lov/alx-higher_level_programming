ae2ddc58501dbe73869fbe0e62a191fa73db0547
Peace-lov <chidieberepeace42@gmail.com>
2023-12-01 02:41:41 -0800

    python5

---
+++ 0x05-python-exceptions/python.h
@@ -0,0 +1,12 @@
+#ifndef _Python_H
+#define _Python_H
+
+#include <stdio.h>
+#include <stlib.h>
+
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+void print_python_float(PyObject *p);
+
+#endif
