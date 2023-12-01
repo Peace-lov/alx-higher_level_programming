63c630bbaec3ff94f77909f35092e07b99b61fa7
Peace-lov <chidieberepeace42@gmail.com>
2023-12-01 00:45:08 -0800

    python5

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    solution = 0
+    for k in range(1, 3):
+        try:
+            if k > a:
+                raise Exception('Too far')
+            else:
+                solution += a ** b / k
+            except Exception:
+                solution = b + a
+                break
+        return solution
