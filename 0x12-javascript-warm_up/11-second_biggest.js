ecf3567dd959c8e9d9820683641d004fe91b61aa
Peace-lov <chidieberepeace42@gmail.com>
2024-03-15 03:33:42 -0700

    java

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 if (process.argv.length <= 3) {
 	console.log('0');
 } else {
-	const ar = process.argv.slice(2).map(Number);
-	const second = ar.sort(function (a, b) { return b - a; }) [1];
+	const arr = process.argv.slice(2).map(Number);
+	const second = arr.sort(function (a, b) { return b - a; }) [1];
 	console.log(second);
 }
