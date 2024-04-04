0c5cf9b3fca2c51cfd9ee14f827582d84e62d3b1
Peace-lov <chidieberepeace42@gmail.com>
2024-04-04 00:59:09 -0700

    python-network0

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-"""Defines a peak-finding algorithm"""
+""" Defines a peak-finding algorithm
+"""
+
 
 def find_peak(list_of_integers):
-    """Return a peak in a list of unsorted integers."""
+    """ Return a peak in a list of unsorted integers.
+    """
+
     if list_of_integers == []:
         return None
 
